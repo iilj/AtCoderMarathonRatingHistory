bf249eb0b4698286f58d098c59c085b635d465a0
iilj <u.eeoou@gmail.com>
2021-07-20 19:10:38 +0900

    Create ranking page

--- atcoder-marathon-rating-history-frontend/src/components/NavigationBar.tsx
+++ atcoder-marathon-rating-history-frontend/src/components/NavigationBar.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavLink,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faChartLine,
+  faSortAmountDown,
+} from '@fortawesome/free-solid-svg-icons';
 
 export const NavigationBar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <Navbar color="light" light expand="md" fixed="top">
       <NavbarBrand tag={Link} to={'/'}>
         AtCoder Marathon Rating History
       </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink tag={Link} to="/rating/">
+              <FontAwesomeIcon
+                style={{ marginRight: '4px' }}
+                icon={faChartLine}
+              />
+              Rating
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/ranking/">
+              <FontAwesomeIcon
+                style={{ marginRight: '4px' }}
+                icon={faSortAmountDown}
+              />
+              Ranking
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
     </Navbar>
   );
 };
---
+++ atcoder-marathon-rating-history-frontend/src/pages/ranking/index.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import useSWR from 'swr';
+import { Alert } from 'reactstrap';
+import { Contest } from '../../interfaces/Contest';
+import {
+  ContestResults,
+  fetchContestResults,
+  fetchContests,
+} from '../../utils/Data';
+import { RankingEntry, RatingRanks } from '../../utils/Rating';
+import { RankingTable } from './RankingTable';
+
+export const RankingPage: React.FC = () => {
+  const { data: contests, error: contestsError } = useSWR<Contest[], Error>(
+    '/json/contests/contests',
+    fetchContests
+  );
+  const { data: contestResultsArray, error: contestResultsArrayError } = useSWR<
+    (ContestResults | undefined)[],
+    Error
+  >(contests ? `/json/results/` : null, () => {
+    if (!contests) return Promise.resolve([undefined]);
+    const promises = contests.map((contest) =>
+      fetchContestResults(contest.slug)
+    );
+    return Promise.all(promises);
+  });
+
+  const ratingRanks = new RatingRanks(contestResultsArray);
+  // {rank, rating, username}[]
+  const ranking: RankingEntry[] = ratingRanks.getRanking();
+
+  return (
+    <>
+      <h2>Description</h2>
+      <p>AtCoder で行われたマラソンコンテストの総合ランキングを表示します．</p>
+      <p>
+        AtCoder
+        が公式にマラソンレートの総合ランキング表示機能を実装するまでのつなぎです．
+      </p>
+      <hr />
+
+      {contestsError ? (
+        <Alert
+          color="danger"
+          style={{
+            marginTop: '50px',
+            marginBottom: '50px',
+          }}
+        >
+          Failed to fetch contest list.
+        </Alert>
+      ) : contests === undefined ? (
+        <div
+          style={{
+            width: '100%',
+            height: '500px',
+            textAlign: 'center',
+            marginTop: '100px',
+            marginBottom: '100px',
+          }}
+        >
+          Fetch contest data...
+        </div>
+      ) : contestResultsArrayError ? (
+        <Alert
+          color="danger"
+          style={{
+            marginTop: '50px',
+            marginBottom: '50px',
+          }}
+        >
+          Failed to fetch contest results.
+        </Alert>
+      ) : contestResultsArray === undefined ? (
+        <div
+          style={{
+            width: '100%',
+            height: '500px',
+            textAlign: 'center',
+            marginTop: '100px',
+            marginBottom: '100px',
+          }}
+        >
+          Fetch contest results...
+        </div>
+      ) : (
+        <></>
+      )}
+      <RankingTable ranking={ranking} />
+    </>
+  );
+};
